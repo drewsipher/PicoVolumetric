ecafaf405d92eead7f6a11bcc42ea0e98f797e51
Drew Shark <drew@matterandform.net>
2021-05-03 15:02:36 -0400

    starting to write code for a single screen

--- main.cpp
+++ main.cpp
@@ -3,50 +3,43 @@
 #include "pico/binary_info.h"
 #include "hardware/i2c.h"
 
+#define D0 0;
+#define D1 1;
+#define D2 2;
+#define D3 3;
+#define D4 4;
+#define D5 5;
+#define D6 6;
+#define D7 7;
 
-// I2C reserves some addresses for special purposes. We exclude these from the scan.
-// These are any addresses of the form 000 0xxx or 111 1xxx24 
-bool reserved_addr(uint8_t addr)
-{
-    return(addr&0x78) == 0 || (addr&0x78) == 0x78;
-}
+#define CS 28;
+#define DC 27;
+#define RST 26;
+#define EN 22;
+#define RW 21;
 
 int main() {
     stdio_init_all();
-    while (true) {
-        i2c_init(i2c_default,100*1000);
-        gpio_set_function(PICO_DEFAULT_I2C_SDA_PIN,GPIO_FUNC_I2C);
-        gpio_set_function(PICO_DEFAULT_I2C_SCL_PIN,GPIO_FUNC_I2C);
-        gpio_pull_up(PICO_DEFAULT_I2C_SDA_PIN);
-        gpio_pull_up(PICO_DEFAULT_I2C_SCL_PIN);
 
-        // Make the I2C pins available to picotool 4 and 5
-        bi_decl(bi_2pins_with_func(PICO_DEFAULT_I2C_SDA_PIN,PICO_DEFAULT_I2C_SCL_PIN,GPIO_FUNC_I2C));
-        
-        printf("\nI2C Bus Scan\n");
-        printf("   0  1  2  3  4  5  6  7  8  9  A  B  C  D  E  F\n");
-        for(int addr = 0; addr < (1<<7); ++addr)
-        {
-            if (addr % 16 == 0)
-            {
-                printf("%02x ",addr);
-            }
-            // Perform a 1-byte dummy read from the probe address. If a slave
-            // acknowledges this address, the function returns the number of bytes
-            // transferred. If the address byte is ignored, the function returns
-            // -1.56 
-            // Skip over any reserved addresses.
-            int ret;
-            uint8_t rxdata;
-            if(reserved_addr(addr))
-                ret = PICO_ERROR_GENERIC;
-            else 
-                ret = i2c_read_blocking(i2c0,addr,&rxdata,1,false);
-            
-            printf(ret<0?".":"@");
-            printf(addr%16==15?"\n":"  ");
-        }
-        
+    uint32_t outputMask = 0x0000;
+    outputMask |= 1 << D0;
+    outputMask |= 1 << D1;
+    outputMask |= 1 << D2;
+    outputMask |= 1 << D3;
+    outputMask |= 1 << D4;
+    outputMask |= 1 << D5;
+    outputMask |= 1 << D6;
+    outputMask |= 1 << D7;
+
+    outputMask |= 1 << CS;
+    outputMask |= 1 << DC;
+    outputMask |= 1 << RST;
+    outputMask |= 1 << EN;
+    outputMask |= 1 << RW;
+
+    gpio_set_dir_out_masked(outputMask);
+    
+    while (true) {
         sleep_ms(10000);
     }
     return 0;
