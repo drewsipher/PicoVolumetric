a45dd17fd0aa703e355b4516e138b3659c403fc8
Drew Shark <drew@matterandform.net>
2021-07-10 21:29:50 -0400

    working 10 screens

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <math.h>
+#include <vector>
 #include "pico/stdlib.h"
 #include "pico/binary_info.h"
 
@@ -12,7 +13,19 @@
 #define D6 8
 #define D7 9
 
-#define CS 16
+const uint32_t CS12 = 1 << 16;
+const uint32_t CS11 = 1 << 17;
+const uint32_t CS10 = 1 << 18;
+const uint32_t CS09 = 1 << 21;
+const uint32_t CS08 = 1 << 22;
+const uint32_t CS07 = 1 << 28;
+const uint32_t CS06 = 1 << 10;
+const uint32_t CS05 = 1 << 11;
+const uint32_t CS04 = 1 << 12;
+const uint32_t CS03 = 1 << 13;
+const uint32_t CS02 = 1 << 14;
+const uint32_t CS01 = 1 << 15;
+
 #define DC 20
 #define RST 19
 #define EN 27
@@ -20,32 +33,49 @@
 
 #define LED 25
 
+#define numberOfScreens 10
 uint32_t pinMaskData = 0;
+uint32_t CS_All = CS01 | CS02 | CS03 | CS04 | CS05 | CS06 | CS07 | CS08 | CS09 | CS10 | CS11 | CS12;
+uint32_t CSToggle = 0;
+uint32_t CSbyIndex[numberOfScreens] = {CS01, CS02, CS03, CS04, CS05, CS06, CS07, CS08, CS09, CS10};
 
 static inline void cs_select() {
     asm volatile("nop \n nop \n nop");
-    gpio_put(CS, 0);  // Active low
+    gpio_put_masked(CS_All, ~CSToggle);
+    // gpio_put(csArray[index], 0);  // Active low
     asm volatile("nop \n nop \n nop");
 }
 
 static inline void cs_deselect() {
     asm volatile("nop \n nop \n nop");
-    gpio_put(CS, 1);
+    gpio_put_masked(CS_All, CS_All);
+    // gpio_put(csArray[index], 1);
     asm volatile("nop \n nop \n nop");
 }
 
+static inline void deselectAll(){
+    CSToggle = 0;
+    cs_deselect();
+}
+
+static inline void selectAll(){
+    CSToggle = CS_All;
+    cs_select();
+}
+
+static inline void selectScreen(uint8_t screen)
+{
+    CSToggle = CSbyIndex[screen];
+    cs_select();
+}
+
 static void write_byte(uint8_t byte) {
-    uint8_t buf[1];
-    // buf[0] = reg & 0x7f;  // remove read bit as this is a write
-    // buf[1] = data;
-    buf[0] = byte;
     cs_select();
     
     uint32_t val = 0x0000 | (byte << D0);
     gpio_put_masked(pinMaskData, val);
     
     cs_deselect();
-    // sleep_ms(10);
 }
 
 void writeCommand(uint8_t command)
@@ -93,12 +123,14 @@ void OLED_init()
 {
     // gpio_put(EN, false);
     // gpio_put(CS, false); //clear Chip Select
-    cs_deselect();
+    deselectAll();
 
     gpio_put(RST, false);
     sleep_ms(1);
     gpio_put(RST, true);
     sleep_ms(120);
+
+    selectAll();    
 
     writeCommand(0xFD);
     writeCommand(0x12);
@@ -126,6 +158,8 @@ void OLED_init()
     Fill_RAM(0x00);
 
     writeCommand(0xAF);
+
+    deselectAll();
 }
 
 
@@ -145,8 +179,8 @@ void Blink(int count, uint32_t delay)
 int main() {
     stdio_init_all();
     pinMaskData = 1 << D0 | 1 << D1 | 1 << D2 | 1 << D3 | 1 << D4 | 1 << D5 | 1 << D6 | 1 << D7;
-    uint32_t pinMaskAll =  1 << LED | 1 << DC | 1 << RST | 1 << EN | 1 << CS | 1 << RW | pinMaskData;
-
+    uint32_t pinMaskAll =  1 << LED | 1 << DC | 1 << RST | 1 << EN | 1 << RW | pinMaskData | CS_All;
+    
     gpio_init_mask(pinMaskAll);
     gpio_set_dir_out_masked(pinMaskAll);
 
@@ -154,35 +188,46 @@ int main() {
     OLED_init();
     Blink(3,150);
     
+    uint8_t screenCount = 12;
+    
     while (true) {
-        for (int i = 0; i < 128; i++)
+        
+        screenCount++;
+        if (screenCount >= numberOfScreens)
         {
-            for (int y = 0; y < 8; y++)
-            {
-                SetStartPage(y);
-                SetStartColumn(i);
-                writeData(0xff);
-            }
-            sleep_ms(10);
+            screenCount = 0;
         }
-        Fill_RAM(0x00);
+        selectScreen(screenCount);
 
-        for (int y = 7; y >= 0; y--)
-        {
-            SetStartPage(y);
-            uint8_t pixelVal = 0;
-            for (int p = 7; p >= 0; p--)
+          for (int i = 0; i < 128; i++)
             {
-                pixelVal |= 1 << p;
-                for (int i = 0; i < 128; i++)
+                for (int y = 0; y < 8; y++)
                 {
+                    SetStartPage(y);
                     SetStartColumn(i);
-                    writeData(pixelVal);
+                    writeData(0xff);
                 }
                 sleep_ms(10);
             }
-        }
-        Fill_RAM(0x00);
+            Fill_RAM(0x00);
+
+            for (int y = 7; y >= 0; y--)
+            {
+                SetStartPage(y);
+                uint8_t pixelVal = 0;
+                for (int p = 7; p >= 0; p--)
+                {
+                    pixelVal |= 1 << p;
+                    for (int i = 0; i < 128; i++)
+                    {
+                        SetStartColumn(i);
+                        writeData(pixelVal);
+                    }
+                    sleep_ms(10);
+                }
+            }
+            Fill_RAM(0x00);
+        
     }
     return 0;
 }
